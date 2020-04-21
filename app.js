9210b11f1a2ec5f919fde7aa7f34c6a127d1e478
Fayk Chaudhry <faykc123@gmail.com>
2020-04-20 23:14:47 -0400

    Initial working model of a Slack Application that responds to hello using the Slack Bolt API

---
+++ app.js
@@ -0,0 +1,40 @@
+const { App } = require('@slack/bolt');
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+
+// This will listen for messages with hello written to them
+app.message('hello', async ({ message, say }) => {
+  await say({
+    blocks: [
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": `Hey there <@${message.user}>!`
+      },
+      "accessory": {
+        "type": "button",
+        "text": {
+          "type": "plain_text",
+          "text": "Click Me"
+        },
+        "action_id": "button_click"
+      }
+    }
+    ]
+  });
+});
+
+
+(async () => {
+  // Start your app
+  await app.start(3000);
+  console.log(`⚡️ Bolt app is running! on `);
+})();
+
+
